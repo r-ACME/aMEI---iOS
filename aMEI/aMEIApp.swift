919eee767ac1f5e315741edc12b771a7a0083185
Brenda Sales <brendasales1997@gmail.com>
2023-06-12 16:04:07 -0300

    Initial Commit

---
+++ aMEI/aMEIApp.swift
@@ -0,0 +1,17 @@
+//
+//  aMEIApp.swift
+//  aMEI
+//
+//  Created by coltec on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct aMEIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
